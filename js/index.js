1a67959acdc7c688eb24b74d779229d931330103
prybula_roman <roman.bismail@gmail.com>
2021-09-21 16:50:15 +0300

    test

--- js/index.js
+++ js/index.js
@@ -9,28 +9,26 @@ const body = document.querySelector("body");
 //console.log(localStorage.getItem("theme"));
 
 body.classList.add(localStorage.getItem("theme"));
-
 const inp = document.querySelector(".theme-switch__toggle");
+//console.log("+++", localStorage.getItem("isChecked"));
+inp.checked = JSON.parse(localStorage.getItem("isChecked"));
 //console.dir(inp);
 inp.addEventListener("click", () => {
   if (!body.classList.contains("dark-theme")) {
     localStorage.setItem("theme", "dark-theme");
     localStorage.setItem("isChecked", "true");
-
     console.log(inp.classList);
     body.classList.add(localStorage.getItem("theme"));
     console.log("++++", localStorage.getItem("isChecked"));
-    inp.checked = true;
-    // console.log("++++", inp.checked);
+    inp.checked = JSON.parse(localStorage.getItem("isChecked"));
+    console.dir(inp);
   } else {
     localStorage.setItem("theme", "light-theme");
-
     console.log(inp.classList);
-    localStorage.setItem("checked", "false");
-
+    localStorage.setItem("isChecked", "false");
     body.classList.remove("dark-theme");
     console.log("----", localStorage.getItem("isChecked"));
-    inp.checked = false;
-    // console.log("----", inp.checked);
+    inp.checked = JSON.parse(localStorage.getItem("isChecked"));
+    console.dir(inp);
   }
 });
